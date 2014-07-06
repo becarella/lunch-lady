7c0f31e03536319cbe71c3d0bace4ffa27c2acaa
Becky Carella <becarella@gmail.com>
2014-07-05 23:45:32 -0400

    remove protect_from_forgery

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -5,7 +5,16 @@ describe OrdersController do
 
   describe 'new_email' do
     before do
-      post :new_email, {from: 'becarella@gmail.com', to: 'test@example.com', subject: 'cheers!', body: 'making your way in the world today sure does take a lot'}
+      post :new_email, {
+        envelope: {
+          from: 'becarella@gmail.com', 
+          to: 'test@example.com'
+        }, 
+        headers: {
+          subject: 'cheers!'
+        }, 
+        html: 'making your way in the world today sure does take a lot'
+      }
     end
 
     specify { expect(response).to be_ok }
