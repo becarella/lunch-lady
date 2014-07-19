6fee270ec4cd05035b450c5555ddc3fe8bc463dc
Becky Carella <becarella@gmail.com>
2014-07-19 19:00:22 -0400

    debug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,11 +3,11 @@ class OrdersController < ApplicationController
   def new_email
     # user = VenmoUser.find_by_email(params[:envelope][:from])
     Rails.logger.info "NEW_EMAIL WRITING TO FILE #{params[:html].encoding}"
-    File.write('/tmp/seamless_order.html', params[:html].force_encoding("UTF-8"))
+    File.open('/tmp/seamless_order.html', 'wb') do |file|
+      file << params[:html]
+    end
     Rails.logger.info "NEW_EMAIL WROTE TO FILE"
-    f = File.new('/tmp/seamless_order.html')
-    Rails.logger.info "NEW_EMAIL SAVING TO S3"
-    f.save_to_s3('lunch_lady', 'orders', 'seamless_order.html')
+    File.new('/tmp/seamless_order.html').save_to_s3('lunch_lady', 'orders', 'seamless_order.html')
     Rails.logger.info "NEW_EMAIL SAVED TO S3"
     # Order.from_seamless!(user, params[:html])
     # Rails.logger.info "ORDER: #{order.inspect}"
