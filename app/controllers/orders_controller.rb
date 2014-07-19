ceecdb22bbf6e8bcb5230d2b1ad1ab1015820c40
Becky Carella <becarella@gmail.com>
2014-07-19 17:55:36 -0400

    more logging

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,9 @@ class OrdersController < ApplicationController
   def new_email
     user = VenmoUser.find_by_email(params[:envelope][:from])
     Rails.logger.info "USER: #{user.inspect}"
+    Rails.logger.info "HTML:"
+    Rails.logger.info params[:html]
+    
     Order.from_seamless!(user, params[:html])
     Rails.logger.info "ORDER: #{order.inspect}"
     render json: params
