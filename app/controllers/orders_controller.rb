dd4ab5079b29b249218505fb37c087f0fe874a82
Becky Carella <becarella@gmail.com>
2014-07-19 18:24:16 -0400

    debug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,11 +2,13 @@ class OrdersController < ApplicationController
 
   def new_email
     # user = VenmoUser.find_by_email(params[:envelope][:from])
-    # File.write('/tmp/seamless_order.html', params[:html])
-    # Rails.logger.info 'saving file...'
-    # f = File.new('/tmp/seamless_order.html')
-    # f.save_to_s3('lunch_lady', 'orders', 'seamless_order.html')
-    # Rails.logger.info 'saved file...'
+    Rails.logger.info "NEW_EMAIL WRITING TO FILE"
+    File.write('/tmp/seamless_order.html', params[:html])
+    Rails.logger.info "NEW_EMAIL WROTE TO FILE"
+    f = File.new('/tmp/seamless_order.html')
+    Rails.logger.info "NEW_EMAIL SAVING TO S3"
+    f.save_to_s3('lunch_lady', 'orders', 'seamless_order.html')
+    Rails.logger.info "NEW_EMAIL SAVED TO S3"
     # Order.from_seamless!(user, params[:html])
     # Rails.logger.info "ORDER: #{order.inspect}"
     # render json: params
