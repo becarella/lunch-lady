40df36dac2f66a71ce335b09de0acb679ee32465
Becky Segal <becarella@gmail.com>
2015-06-27 14:11:43 -0400

    updating parser, still very broken

--- app/services/seamless.rb
+++ app/services/seamless.rb
@@ -27,7 +27,7 @@ class Seamless
     @user = user
   end
 
-  def create_order
+  def parse
     json = parse_text
     order = nil
     Order.transaction do
@@ -36,23 +36,24 @@ class Seamless
       order_json[:user_id] = user.id
       order = Order.find_or_create_by(order_json)
       json[:items].each do |item_json|
-        item_json = json.dup.reject { |k,v| !Item.column_names.include?(k.to_s) }
+        item_json = json.dup.reject { |k,v| !LineItem.column_names.include?(k.to_s) }
         item_json[:order_id] = order.id
-        order.items << Item.find_or_create_by(item_json)
+        puts "ITEM JSON: #{item_json.inspect}"
+        order.line_items << LineItem.find_or_create_by(item_json)
       end
     end
-    order
+    order.reload
   end
 
   def parse_text
     doc = Nokogiri::HTML(html)
     data = {site: 'Seamless'}
-    data[:order_number] = doc.search("[text()*='Order']").first.content.match(/\d+/)[0]
-    data[:restaurant] = doc.search("[text()*='Order']").first.ancestors('tr').first.css('td').first.content.strip.split(/\n/).first.strip
-    data[:subtotal] = doc.search("[text()*='Product Total']").first.ancestors('tr').search("[text()*='$']").first.content.gsub('$', '').strip.to_f
+    data[:order_number] = doc.search("[text()*='Order #']").first.content.match(/\d+/)[0]
+    data[:restaurant] = doc.search('h4').first.content
+    data[:subtotal] = doc.search("[text()*='Subtotal']").first.ancestors('tr').search("[text()*='$']").first.content.gsub('$', '').strip.to_f
     data[:tax] = doc.search("[text()*='Sales Tax']").first.ancestors('tr').search("[text()*='$']").first.content.gsub('$', '').strip.to_f
     begin
-      data[:tip] = doc.search("[text()*='Tip']").first.ancestors('tr').search("[text()*='$']").first.content.gsub('$', '').strip.to_f
+      data[:tip] = doc.search("[text()*='Tip Amount']").first.ancestors('tr').search("[text()*='$']").first.content.gsub('$', '').strip.to_f
     rescue
       data[:tip] = 0.0
     end
@@ -61,20 +62,18 @@ class Seamless
     rescue
       data[:discount] = 0.0
     end
-    data[:total] = doc.search("[text()*='Grand Total']").first.ancestors('tr').search("[text()*='$']").first.content.gsub('$', '').strip.to_f
+    data[:total] = doc.search("[text()*='Total']").first.ancestors('tr').search("[text()*='$']").first.content.gsub('$', '').strip.to_f
 
     items = [];
     count = 0;
-    doc.search("[text()*='Special Instructions']").each do |node|
-      order = node.ancestors('tr').first.parent.css('tr')
-      current_item = {
-        charge_to_nickname: node.content.strip.match(/\b[A-Z]+\b/)[0],
-        description: order.css('td')[2].content.strip,
-        subtotal: order.css('td')[6].content.strip.gsub('$', '').to_f
-      }
-      items.push(current_item)
+    doc.search("[text()*='$']").first.ancestors('table').first.ancestors('tbody').first.search('> tr').each do |node|
+      items.push({
+        charge_to_nickname: node.search("[text()*='Special Instructions']").first.content.strip.match(/\b[A-Z]+\b/)[0],
+        description: node.search('td')[1].content.strip,
+        subtotal: node.search('td')[2].content.strip.gsub('$', '').to_f
+      })
     end
-    
+    puts "ITEMS: #{items.inspect}"
     data[:items] = items
     data
   end
